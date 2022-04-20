12fb142e12abafc3f2f2b7f0afb3a962a4ba2aaf
Maria Sattrup <maria.sattrup@gmail.com>
2022-04-21 01:57:23 +0200

    front-page

--- twenty-special/front-page.php
+++ twenty-special/front-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The template for displaying frontpage
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
@@ -14,6 +14,7 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			Dette er min nye forside
 
 			<?php
 
@@ -21,7 +22,7 @@ get_header();
 			while ( have_posts() ) :
 				the_post();
 
-				get_template_part( 'template-parts/content/content', 'page' );
+				get_template_part( 'template-parts/content/content', 'front' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) {
