00d0717996fd3af36e656f298f3812e22f9895c3
Maria Sattrup <maria.sattrup@gmail.com>
2022-04-21 03:47:58 +0200

    forsidebillede

--- twenty-special/front-page.php
+++ twenty-special/front-page.php
@@ -14,23 +14,17 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-			Dette er min nye forside
+			<section id="first_section">
+				<div class="section_wrapper">
+					<h1> UNESCO VERDENSMÅLSSKOLER</h1>
+					<p>80 danske skoler deltager aktivt i UNESCOs verdensomspændende skolenetværk med fokus på undervisningsforløb og materialer, der sætter Globalt medborgerskab og Bæredygtig udvikling på elevers og læreres dagsorden.
+Hjemmesiden giver dig mulighed for at lære mere om netværket, mens du udforsker spændende undervisningsmateriale og bliver inspireret af de mange projekter, som de danske UNESCO Verdensmålsskoler løbende deler.</p>
+				</div>
+				
+		</section>
+		
 
-			<?php
-
-			// Start the Loop.
-			while ( have_posts() ) :
-				the_post();
-
-				get_template_part( 'template-parts/content/content', 'front' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-
-			endwhile; // End the loop.
-			?>
+	
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
