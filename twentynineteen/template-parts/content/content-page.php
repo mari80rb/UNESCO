6efaf957c9ee7ce1a65a205fdcc54b5c86de8d69
Maria Sattrup <maria.sattrup@gmail.com>
2022-04-21 10:15:32 +0200

    peter vejledning

--- twentynineteen/template-parts/content/content-page.php
+++ twentynineteen/template-parts/content/content-page.php
@@ -12,10 +12,9 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-Min særlige forside
 	<?php if ( ! twentynineteen_can_show_post_thumbnail() ) : ?>
 	<header class="entry-header">
-		<?php get_template_part( 'template-parts/header/entry', 'frontheader' ); ?>
+		<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
 	</header>
 	<?php endif; ?>
 
